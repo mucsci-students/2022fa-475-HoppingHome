994df9dc2051d0422a440f2340ecaa59a10d4edb
Justin Stevens <jmms5757@gmail.com>
2022-10-25 09:21:36 -0400

    added camera lock on fall

--- Assets/Scripts/Camera2DFollow.cs
+++ Assets/Scripts/Camera2DFollow.cs
@@ -10,6 +10,7 @@ namespace UnityStandardAssets._2D
         public float lookAheadFactor = 3;
         public float lookAheadReturnSpeed = 0.5f;
         public float lookAheadMoveThreshold = 0.1f;
+        public float yLockBottom = 0f;
 
         private float m_OffsetZ;
         private Vector3 m_LastTargetPosition;
@@ -47,6 +48,10 @@ namespace UnityStandardAssets._2D
             Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
             Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
 
+            if (newPos.y < yLockBottom)
+            {
+                newPos.y = yLockBottom;
+            }
             transform.position = newPos;
 
             m_LastTargetPosition = target.position;
