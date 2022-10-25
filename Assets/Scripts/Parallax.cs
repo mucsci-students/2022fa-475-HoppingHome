19f93f1fb52e9a0d9928c7af2fa9c73efac0fcc5
Samantha Noggle <44234583+astruxie@users.noreply.github.com>
2022-10-25 17:02:43 -0400

    Infinite parallaxing now (slightly laggy)

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    private float lenght, starpos;
-    public GameObject came;
+    private float length, startpos;
+    public GameObject cam;
     public float parallaxEffect;
 
     void Start()
     {
-        starpos = transform.position.x;
-        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float temp = (came.transform.position.x * (1 - parallaxEffect));
-        float distance = (came.transform.position.x * parallaxEffect);
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float distance = (cam.transform.position.x * parallaxEffect);
 
-        transform.position = new Vector3(starpos + distance, transform.position.y, transform.position.z);
+        transform.position = new Vector3(startpos + distance, transform.position.y, transform.position.z);
 
-        if (temp > starpos + lenght) starpos += lenght;
-        else if (temp < starpos - lenght) starpos -= lenght;
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
        
          
     }
