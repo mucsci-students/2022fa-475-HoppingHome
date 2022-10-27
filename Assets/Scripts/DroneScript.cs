fe9ce6940dc30092f0ed8157e42bacf4bae3ea64
Justin Stevens <jmms5757@gmail.com>
2022-10-27 14:21:55 -0400

    updated drone

--- Assets/Scripts/DroneScript.cs
+++ Assets/Scripts/DroneScript.cs
@@ -7,35 +7,66 @@ public class DroneScript : MonoBehaviour
 
     public float speed = 3f;
     public int health = 3;
-    public bool isRight = false;
+    public bool angered = false;
     public Transform leftBound;
     public Transform rightBound;
+
+    private float lb;
+    private float rb;
     private Transform start;
-    private bool angered = false;
+    private bool isRight = false;
     private GameObject player;
     private Rigidbody2D rb2D;
+    private SpriteRenderer sr;
+
+    public GameObject bullet;
+    public float bulletSpeed = 1f;
+    public float bulletCooldown = 2.00f;
+    private float timer = 0f;
 
     void Start()
     {
         start = transform;
         rb2D = GetComponent<Rigidbody2D>();
-        leftBound.position = transform.position + leftBound.position;
-        rightBound.position = transform.position + rightBound.position;
+        lb = leftBound.position.x;
+        rb = rightBound.position.x;
+        sr = gameObject.GetComponent<SpriteRenderer>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (!angered)
-        //{
-            if ((isRight && transform.position.x > rightBound.position.x) || 
-                (!isRight && transform.position.x < leftBound.position.x))
+        float xOffSet = -0.5f;
+        if (!angered)
+        {
+            if ((isRight && transform.position.x >= rb) || 
+                (!isRight && transform.position.x <= lb))
             {
                 flip();
             }
-            // Move the character
-            rb2D.velocity = new Vector2(speed, 0);
-        //}
+            if (isRight)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, new Vector2(rb, transform.position.y), speed * Time.deltaTime);
+            } else {
+                transform.position = Vector2.MoveTowards(transform.position, new Vector2(lb, transform.position.y), speed * Time.deltaTime);
+            }
+        } else {
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(player.transform.position.x + xOffSet, player.transform.position.y + 0.75f), speed * Time.deltaTime);
+            if ((isRight && transform.position.x > player.transform.position.x + xOffSet) || 
+                (!isRight && transform.position.x < player.transform.position.x + xOffSet))
+            {
+                flip();
+            }
+            
+            //Shoot
+            if (timer > bulletCooldown)
+            {
+                fire();
+                timer = 0f;
+            }
+
+            timer += Time.deltaTime;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
@@ -47,13 +78,13 @@ public class DroneScript : MonoBehaviour
         }
     }
 
-    void OnTriggerExit2D(Collider2D col)
-    {
-        if (col.gameObject.name == "Ziggy")
-        {
-            angered = false;
-        }
-    }
+    // void OnTriggerExit2D(Collider2D col)
+    // {
+    //     if (col.gameObject.name == "Ziggy")
+    //     {
+    //         angered = false;
+    //     }
+    // }
 
     private void flip()
     {
@@ -61,6 +92,26 @@ public class DroneScript : MonoBehaviour
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
+        // Fix position on flip
+        transform.position = isRight 
+            ? new Vector3(transform.position.x + sr.bounds.size.x, transform.position.y, transform.position.y)
+            : new Vector3(transform.position.x - sr.bounds.size.x, transform.position.y, transform.position.y);
         isRight = !isRight;
+    }
+
+    private void fire()
+    {
+        Vector3 offsetRight = new Vector3 (sr.bounds.size.x / 2, -1.5f, 1.0f);
+        Vector3 offsetLeft = new Vector3 (-sr.bounds.size.x / 2, -1.5f, 1.0f);
+
+        if (isRight)
+        {
+            GameObject temp = Instantiate(bullet, transform.position + offsetRight, Quaternion.identity);
+            temp.GetComponent<Rigidbody2D>().velocity = (player.transform.position - temp.transform.position).normalized * bulletSpeed;
+        } else {
+            GameObject temp = Instantiate(bullet, transform.position + offsetLeft, Quaternion.identity);
+            temp.GetComponent<Rigidbody2D>().velocity = (player.transform.position - temp.transform.position).normalized * bulletSpeed;
+        }
+        //m_Anim.SetTrigger("Shoot");
     }
 }
