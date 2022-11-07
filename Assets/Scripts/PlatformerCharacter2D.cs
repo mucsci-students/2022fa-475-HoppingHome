763b5570315fc878def9be3f619da1f0e3087c7e
Justin <jmms5757@gmail.com>
2022-11-06 22:38:06 -0500

    bug fix

--- Assets/Scripts/PlatformerCharacter2D.cs
+++ Assets/Scripts/PlatformerCharacter2D.cs
@@ -67,7 +67,7 @@ namespace UnityStandardAssets._2D
             Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
             for (int i = 0; i < colliders.Length; i++)
             {
-                if (colliders[i].gameObject != gameObject)
+                if (colliders[i].gameObject != gameObject && colliders[i].gameObject.tag != "Damage")
                 {
                     m_Grounded = true;
                     break;
